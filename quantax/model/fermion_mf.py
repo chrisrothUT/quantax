54c0a5f909557eff633b5dc0df02bcaff001fb80
Christopher Roth <croth@rustyamd1.cm.cluster>
2025-08-09 06:58:09 -0400

    Revert "changes"
    This reverts commit 041ea054a9b57b087b624cc4f46c8cb916a77e7e.

--- quantax/model/fermion_mf.py
+++ quantax/model/fermion_mf.py
@@ -452,8 +452,12 @@ def _low_rank_update_pfaffian(
         return old_psi * rat * parity
 
 def in_unit_cell(ind,tol=1e-5):
+    c1 = ind[0] > -tol
+    c2 = ind[0] < 1-tol
+    c3 = ind[1] > -tol
+    c4 = ind[1] < 1-tol
     
-    return np.all(ind > -tol)*np.all(ind < 1-tol)
+    return c1*c2*c3*c4
   
 
 def _get_pfaffian_indices(sublattice, N):
